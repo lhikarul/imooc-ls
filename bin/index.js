ea89084f9e231f3bc6097b98eb3fcfbe31c5dc30
lhikarul <711hikaru@gmail.com>
2022-08-20 22:00:25 +0800

    feat: 遍歷當前目錄下的文件

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,17 @@
 #!/usr/bin/env node
 
+const fs = require("fs");
 const parse = require("./parseArgs");
+
+const dir = process.cwd();
 
 const { args, isAll, isList, isDevelopment } = parse();
 
-console.log(args, isAll, isList, isDevelopment);
+if (!isAll && !isList && !isDevelopment) {
+  // 排除以 . 開頭的文件
+  let files = fs.readdirSync(dir);
+  files = files.filter((file) => file.indexOf(".") !== 0);
+  let output = "";
+  files.forEach((file) => (output += file + "             "));
+  console.log(output);
+}
