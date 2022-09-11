935bafd2e0decec4de15f23c4b0af86b1578bc02
lhikarul <711hikaru@gmail.com>
2022-09-11 20:51:19 +0800

    feat: 權限自動化測試

--- bin/getFileType.js
+++ bin/getFileType.js
@@ -1,15 +1,17 @@
 const fs = require("fs");
 
 module.exports = function getFileType(mode) {
-  const isDirectory = mode & fs.constants.S_IFDIR;
-  const isFile = mode & fs.constants.S_IFREG;
-  const isLink = mode & fs.constants.S_IFLNK;
+  const isDirectory = (mode & fs.constants.S_IFDIR) === fs.constants.S_IFDIR;
+  const isFile = (mode & fs.constants.S_IFREG) === fs.constants.S_IFREG;
+  const isLink = (mode & fs.constants.S_IFLNK) === fs.constants.S_IFLNK;
 
   if (isDirectory) {
     return "d";
-  } else if (isFile) {
-    return "-";
   } else if (isLink) {
     return "l";
+  } else if (isFile) {
+    return "-";
+  } else {
+    return "-";
   }
 };
--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ const assert = require("assert");
 const parseArgs = require("../bin/parseArgs");
 const getFileUser = require("../bin/getFileUser");
 const getFileType = require("../bin/getFileType");
+const getAuth = require("../bin/auth");
 
 describe("imooc-ls", function () {
   describe("parseArgs", function () {
@@ -31,7 +32,57 @@ describe("imooc-ls", function () {
 
   describe("getFileType", function () {
     it("is file", function () {
-      getFileType();
+      const mode = 32768;
+      const res = getFileType(mode);
+      assert.equal(res, "-");
+    });
+    it("is directory", function () {
+      const mode = 16384; // dir
+      const res = getFileType(mode);
+      assert.equal(res, "d");
+    });
+    it("is link", function () {
+      const mode = 40960; // link
+      const res = getFileType(mode);
+      assert.equal(res, "l");
+    });
+    it("block device", function () {
+      const mode = 24576; // block device
+      const res = getFileType(mode);
+      assert.equal(res, "d");
+    });
+  });
+
+  describe("getAuth", function () {
+    it("user rwx------", function () {
+      const mode = 4544; // user rwx
+      const str = getAuth(mode);
+      assert.equal(str, "rwx------");
+    });
+    it("group ---rwx---", function () {
+      const mode = 4152; // user rwx
+      const str = getAuth(mode);
+      assert.equal(str, "---rwx---");
+    });
+    it("other ------rwx", function () {
+      const mode = 4103;
+      const str = getAuth(mode);
+      assert.equal(str, "------rwx");
+    });
+    it("none ------", function () {
+      const mode = 4096; // empty
+      const str = getAuth(mode);
+      assert.equal(str, "---------");
+    });
+    it("bad mode 0", function () {
+      const mode = 0; // empty
+      const str = getAuth(mode);
+      assert.equal(str, "---------");
+    });
+    it("bad mode string", function () {
+      const mode = "0"; // empty
+      const str = getAuth(mode);
+      assert.equal(str, "---------");
     });
   });
 });
